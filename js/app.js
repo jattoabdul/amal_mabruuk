5ff7f676b69d54f11a3d56266c13aee4d8031ebe
Jatto Ade <jattoade@gmail.com>
2017-06-15 12:24:14 +0100

    modified ui

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 200) {
+    $('nav').addClass('sticky');
+  } else {
+    $('nav').removeClass('sticky');
+  }
+});
+
+
 $(document).ready(function(){
     $('.button-collapse').sideNav({
       menuWidth: 250, // Default is 300
@@ -5,9 +14,18 @@ $(document).ready(function(){
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
     });
-
+    var windowHeight = $(window).height();
+    $('.slider').slider({
+    	indicators: false,
+    	height: (windowHeight - 90)
+    });
+    $('.slider').mouseenter(function(){
+    	$('.slider').slider('pause');
+    });
+    $('.slider').mouseleave(function(){
+    	$('.slider').slider('start');
+    });
     $('.carousel').carousel({
-        dist: -200,
         full_width: true
     });
 });
