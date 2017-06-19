74a76eb0e305511e2a9433f4b7bc4aae61fae82b
Abdul-Samii Ajala Olalekan <kgasta@gmail.com>
2017-06-19 07:41:44 +0100

    added about us and our work pages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(document).scrollTop() > 200) {
     $('nav').addClass('sticky');
   } else {
@@ -7,25 +7,26 @@ $(window).scroll(function() {
 });
 
 
-$(document).ready(function(){
-    $('.button-collapse').sideNav({
-      menuWidth: 250, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    });
-    var windowHeight = $(window).height();
-    $('.slider').slider({
-    	indicators: false,
-    	height: (windowHeight - 90)
-    });
-    $('.slider').mouseenter(function(){
-    	$('.slider').slider('pause');
-    });
-    $('.slider').mouseleave(function(){
-    	$('.slider').slider('start');
-    });
-    $('.carousel').carousel({
-        full_width: true
-    });
+$(document).ready(function () {
+  $('.button-collapse').sideNav({
+    menuWidth: 250, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
+  $('.parallax').parallax();
+  var windowHeight = $(window).height();
+  $('.slider').slider({
+    indicators: false,
+    height: (windowHeight - 90)
+  });
+  $('.slider').mouseenter(function () {
+    $('.slider').slider('pause');
+  });
+  $('.slider').mouseleave(function () {
+    $('.slider').slider('start');
+  });
+  $('.carousel').carousel({
+    full_width: true
+  });
 });
